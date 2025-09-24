581e123f5410abe8e60468a8ce923267acfb2a22
Takeshi Fujino <fujino@edu.k.u-tokyo.ac.jp>
2025-09-24 19:24:49 +0900

    wip

--- check_nstm_outputs.rb
+++ check_nstm_outputs.rb
@@ -6,6 +6,8 @@ require 'pathname'
 
 DATA_PROCESSING_OUTPUTS_PATH = Pathname.new(ARGV.shift)
 
+puts %w[sample filename ntsm_score ntsm_result].join("\t")
+
 CSV.table(DATA_PROCESSING_OUTPUTS_PATH, col_sep: "\t", quote_char: "\x00").each do |row|
   sample = row[:sample]
   outputs = JSON.parse(row[:workflow_outputs])['outputs']
@@ -14,18 +16,18 @@ CSV.table(DATA_PROCESSING_OUTPUTS_PATH, col_sep: "\t", quote_char: "\x00").each 
     summary_path = outputs['hifi_qc_wf.hifi_qc_summary']
     summary = CSV.table(summary_path, col_sep: "\t")
     filename = summary[:filename]
+    ntsm_score = summary[:ntsm_score]
     ntsm_result = summary[:ntsm_result]
   when 'data_processing/hic_qc_workflow'
     summary_path = outputs['hic_qc_wf.hic_qc_summary']
     summary = CSV.table(summary_path, col_sep: "\t")
     filename = summary[:file_name]
+    ntsm_score = summary[:ntsm_score]
     ntsm_result = summary[:ntsm_result]
   else
     warn "Unsupported workflow type: #{row[:workflow_name]}"
     exit 1
   end
 
-
-
-  puts [sample, filename, ntsm_result].join("\t")
+  puts [sample, filename, ntsm_score, ntsm_result].join("\t")
 end
